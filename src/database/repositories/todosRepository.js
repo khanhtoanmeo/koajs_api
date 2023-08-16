6f7ce87b5fb9ac6240f0a808bf339ea13150b583
anhth1 <114393245+anhth1@users.noreply.github.com>
2023-08-16 09:50:38 +0700

    update todo backend

--- src/database/repositories/todosRepository.js
+++ src/database/repositories/todosRepository.js
@@ -2,6 +2,7 @@ import db from "../db.js";
 import { pick } from "../../helpers/pick.js";
 
 export async function getAll(params = {}) {
+  //todo: cái này dùng lại nhiều quá 
   let todosRef = db.collection("todos");
 
   const { limit, sort } = params;
@@ -12,6 +13,7 @@ export async function getAll(params = {}) {
 
   const todosSnapshot = await todosRef.get();
   const todos = todosSnapshot.docs.map((doc) => ({
+    //todo: cái này viết thành 1 hàm prepareDocs , để có thể dùng lại nhiều lần 
     ...doc.data(),
     id: doc.id,
   }));
@@ -34,16 +36,20 @@ export async function getOne(id, fields = []) {
 export async function save(data) {
   const todosRef = db.collection("todos");
   const createdAt = new Date();
+  //createAt tham khảo và tìm hiểu để sử dụng cái này thì oke hơn FieldValue.serverTimestamp()
 
   const newTodo = { ...data, isCompleted: false, createdAt };
-
+  //cái này trả về  id không cần thiết get 1 lần nữa 
   const todoRef = await todosRef.add(newTodo);
   const todo = await todoRef.get();
 
+
+//todo: như cái này có thể áp dùng hàm prepareDocs như anh nói ở trên 
   return { ...todo.data(), id: todo.id };
 }
 
 export async function deleteMany(ids) {
+  //todo: cái này cũng như cái dưới cần tìm cách tối ưu hơn nữa 
   const todosRef = db.collection("todos");
   ids.forEach((id) => {
     todosRef.doc(id).delete();
@@ -53,6 +59,7 @@ export async function deleteMany(ids) {
 
 export async function updateMany(todos) {
   const todosRef = db.collection("todos");
+  // todo : anh không nghĩ là nên dùng forEach ở đây , + nên dùng update khi update chứ không dùng set , tìm cách khác tối ưu hơn 
   todos.forEach(async (todo) => {
     const { id, isCompleted } = todo;
     await todosRef.doc(id).set(
